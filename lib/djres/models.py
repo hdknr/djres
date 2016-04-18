f7e8e122057f4183746c5e256091b87d5b791164
hdknr <gmail@hdknr.com>
2016-04-18 09:48:26 +0000

    admin for Lang

--- lib/djres/models.py
+++ lib/djres/models.py
@@ -12,3 +12,19 @@ class BaseModel(models.Model, methods.BaseModel):
 
     class Meta:
         abstract = True
+
+
+class Lang(BaseModel):
+    code = models.CharField(
+        _('Language Code'), max_length=10, unique=True, db_index=True)
+    name_local = models.CharField(
+        _('Language Name'), max_length=20, null=True, blank=True, default=None)
+    name_translated = models.CharField(
+        _('Language Name Translated'),
+        max_length=20, null=True, blank=True, default=None)
+    bidi = models.BooleanField(
+        _('Bi Direction'), default=False)
+
+    class Meta:
+        verbose_name = _('Supported Language')
+        verbose_name_plural = _('Supported Language')
